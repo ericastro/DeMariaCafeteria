90dc21d2722c6395b06795ad64d592ec945c4742
Eric Oliveira de Castro <eric.castro.dev@gmail.com>
2019-12-12 18:24:15 -0300

    Testes Capsulas
    Inclusão de Testes Unitários para Classe Capsula

---
+++ WindowsFormsCafeteria/UnitTestCafeteria/CapsulaTeste.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WindowsFormsCafeteria;
+
+namespace UnitTestCafeteria
+{
+    [TestClass]
+    public class CapsulaTeste
+    {
+        //[Metodo]_[Condicao]_[ResultadoEsperado]
+
+        [TestMethod]
+        public void CapsulaForca_RecebeValorMaiorQue10_DeveRetornarInteiroIgualA0()
+        {
+            //arrange
+            Capsula capsula = new Capsula();
+            capsula.Forca = 20;
+            int resultadoEsperado = 0;
+
+            //Act
+            int resultadoAtual = capsula.Forca;
+            //Assert
+
+            Assert.AreEqual(resultadoEsperado, resultadoAtual);
+        }
+    }
+}
---
+++ WindowsFormsCafeteria/UnitTestCafeteria/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestCafeteria")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestCafeteria")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("56d8d6bb-6200-452a-b289-cf6a55b2dda4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
