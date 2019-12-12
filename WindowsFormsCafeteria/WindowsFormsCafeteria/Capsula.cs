b683606987de36a75f5420b640a4f5d7b088418f
Eric Oliveira de Castro <eric.castro.dev@gmail.com>
2019-12-12 08:08:52 -0300

    Incluido Classe Capsula.cs
    Inclusão dos arquivos para criação da classe Capsula,cs

---
+++ WindowsFormsCafeteria/WindowsFormsCafeteria/Capsula.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsCafeteria
+{
+    class Capsula
+    {
+        public Capsula()
+        {
+
+        }
+
+        public int capsulas_id { get; set; }
+        public string descricao { get; set; }
+        public int forca { get; set; }
+
+        public void IncluirCapsula(Capsula capsula)
+        {
+
+        }
+
+        public void LitarCapsulas()
+        {
+
+        }
+
+        public void EditarCapsulas(Capsula capsula)
+        {
+
+        }
+
+        public void ExcluirCapsulas(int capusla_id)
+        {
+
+        }
+    }
+
+
+
+}
