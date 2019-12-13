bef72a5b7afd316daa025ba0b30dbfb403034e95
Eric Oliveira de Castro <eric.castro.dev@gmail.com>
2019-12-13 16:11:06 -0300

    Update DeMariaCafeteria.sql
    Alterações finais no script de criação de banco de dados, tive varios problemas em rodas o script no pgAdmin4 onde o mesmo script foi criado, por este motivo hoje a noite ainda farei um video executando o programa e exibindo o banco de dados.

--- DeMariaCafeteria.sql
+++ DeMariaCafeteria.sql
@@ -1,6 +1,6 @@
--- Database: Cafeteria
+-- Database: DeMariaEric
 
--- DROP DATABASE "Cafeteria";
+-- DROP DATABASE "DeMariaEric";
 
 CREATE DATABASE "DeMariaEric"
     WITH 
@@ -13,19 +13,30 @@ CREATE DATABASE "DeMariaEric"
 
 
 -- NAO É POSSIVEL EXECUTAR OS COMANDOS ABAIXOS ANTES DE CRIAR O BANDO DE DADOS;
--- NO POSTGRES NÃO É POSSIVEL TROCAR DE BANCO DE DADOS COMO EM OUTROS SGDB USANDO "USE DATABASE"
--- POR ESTE MOTIVO É NESSECARIO FAZER UMA NOVA CONEXAO COM O BANCO DE DADOS CRIADO
--- PELO CONSOLE DE COMANDO USANDO "\connect test;"
--- OU PELO PGADMIN SELECIONANDO O NOVO BANCO DE DADOS
+-- NO POSTGRES NÃO É POSSIVEL TROCAR DE BANCO DE DADOS COMO EM OUTROS SGDB 
+-- USANDO O COMANDO "USE DATABASE" POR EXEMPLO;
+-- POR ESTE MOTIVO É NESSECARIO FAZER UMA NOVA CONEXAO COM O BANCO DE DADOS CRIADO;
+-- PELO CONSOLE DE COMANDO USANDO "\connect test;";
+-- OU PELO PGADMIN SELECIONANDO O NOVO BANCO DE DADOS.
 
-CREATE SCHEMA IF NOT EXISTS "cafeteria" AUTHORIZATION postgres;
+-- SCHEMA: cafeteria
 
-CREATE TABLE IF NOT EXISTS cafeteria.capsulas
+-- DROP SCHEMA cafeteria ;
+
+CREATE SCHEMA cafeteria
+    AUTHORIZATION postgres;
+
+
+-- Table: cafeteria.capsulas
+
+-- DROP TABLE cafeteria.capsulas;
+
+CREATE TABLE cafeteria.capsulas
 (
-	capsula_id serial,
-	descricao varchar(200) NOT NULL,
-	forca integer NOT NULL,
-	PRIMARY KEY (capsula_id)
+    capsula_id integer NOT NULL DEFAULT nextval('cafeteria.capsulas_capsula_id_seq'::regclass),
+    descricao character varying(200) COLLATE pg_catalog."default" NOT NULL,
+    forca integer NOT NULL,
+    CONSTRAINT capsulas_pkey PRIMARY KEY (capsula_id)
 )
 
 TABLESPACE pg_default;
@@ -33,17 +44,29 @@ TABLESPACE pg_default;
 ALTER TABLE cafeteria.capsulas
     OWNER to postgres;
 
-CREATE TABLE IF NOT EXISTS cafeteria.estoque
+
+-- Table: cafeteria.estoque
+
+-- DROP TABLE cafeteria.estoque;
+
+CREATE TABLE cafeteria.estoque
 (
     capsula_id integer NOT NULL,
     data_entrada date NOT NULL,
-    data_saida date NOT NULL
+    data_saida date,
+    validade date NOT NULL,
+    estoque_id integer NOT NULL DEFAULT nextval('cafeteria.estoque_estoque_id_seq'::regclass),
+    CONSTRAINT estoque_pkey PRIMARY KEY (estoque_id)
 )
 
 TABLESPACE pg_default;
 
 ALTER TABLE cafeteria.estoque
     OWNER to postgres;
+
+-- A CRIACAO DA VIEW SERIA NECESSARIA SOMENTE PARA LISTAR AS CAPSULAS
+-- INFELIZMENTE NÃO TIVE TEMPO HÁBIL PARA REALIZAR ESTA TAREFA
+-- PORTANTO ELA NÃO SERÁ NECESSARIA PARA EXECUÇÃO DO PROGRAMA AINDA.    
 
 create view cafeteria.estoque_capsulas as
 select 	A."capsula_id", 
